8a705c0dc9635133fc9f4173f9e0a1cd6f6d71ee
matheussn <matheusneto23@gmail.com>
2017-06-05 01:00:27 -0300

    implementação das funções especificadas no arquivo lista.h

---
+++ lista.c
@@ -0,0 +1,94 @@
+// count
+// -----
+// Return the quantity of items in l;
+// or return -1, if l is not found.
+
+int
+count (void * l)
+{
+
+}
+
+
+// create
+// ------
+// Return a pointer to the created list, whose label is s;
+// or return NULL, if the list was not created
+
+void *
+create (char *s)
+{
+	
+}
+
+
+// destroy
+// -------
+// Return NULL if l was destroyed; return l otherwise.
+// Destroying l implies in deallocating all nodes of l.
+
+void * 
+destroy (void *l)
+{
+
+}
+
+
+// find
+// ----
+// Return a pointer to the item whose id is m;
+// or return NULL, if no item with id m was found.
+
+Item *
+find (void *l, int m)
+{
+	
+}
+
+
+// idl
+// ---
+// Return a pointer to list of items whose label is s;
+// or return NULL, if does not exist a list with label s.
+
+void *
+idl (char *s)
+{
+
+}
+
+
+// insert
+// ------
+// Return l, if m is inserted into l; or return NULL, if
+// either l was not found or space for m was not allocated.
+// m is inserted into l in ascending order.
+
+void *
+insert (void *l, Item m)
+{
+
+}
+
+
+// label
+// -----
+// Return the label of l; or return NULL, if l is not found.
+
+char *
+label (void *l)
+{
+
+}
+
+
+// showAll
+// -------
+// Present all items of l.
+// Return l; or return NULL, if l was not found.
+
+void *
+showAll (void *l)
+{
+
+}
