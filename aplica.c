dc817399a9a731396fdb899846d1d4885d08c2e7
admrodrigo2 <admrodrigo2@yahoo.com.br>
2017-06-10 20:17:04 -0300

    Criando primeira parte da entrega

--- aplica.c
+++ aplica.c
@@ -3,8 +3,56 @@
 
 #include "lista.h"
 
+#define MAXDESCR 30
+
 int
-main (int argc, char ** argv)
+main ()
 {
-	FILE * f;
+	char fn; //Uma letra que representa uma das funções
+	char *rl; //Rótulo da lista.
+	int c1; //Um número.
+	char *c2; //cadeia de caracteres
+    	Lista *dir;
+
+	c2= (char*) malloc(MAXDESCR);
+	if(rl == NULL)
+        {
+		fprintf (stderr, "Erro na alocacao de c2");
+		return -15;
+	}
+
+	printf("Digite uma operacao.\n");
+	while(EOF)
+    	{
+		rl = (char*) malloc(MAXDESCR);
+		if(rl == NULL)
+        {
+		fprintf (stderr, "Erro na alocacao de rl");
+		return -15;
+	}
+		
+        setbuf(stdin,NULL);
+        scanf("%c %s %d %s", &fn, rl, &c1, c2);
+        switch(fn)
+        {
+            case 'c' :
+                dir = create(rl);
+                if(dir == NULL)
+                	fprintf (stderr, "Erro ao criar a lista %s",rl);
+				
+		else
+			printf("Lista %s criada com sucesso!\n", rl);
+			
+            break;
+
+            case 's' :
+                showDir();
+                break;
+
+            default :
+                fprintf (stderr,"Funcao invalida!\n");
+        }
+    }
+
+    return 0;
 }
